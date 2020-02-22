b46e268fe7903c672b44eb7c66e3cd51ca179cb7
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2020-02-22 20:17:58 +0300

    add onChange for input

--- src/components/SingIn/SignIn.tsx
+++ src/components/SingIn/SignIn.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
+import React, {ChangeEvent} from 'react';
 import {connect} from "react-redux";
 import {RootState} from "../../redux/store";
-import {IUser} from "../../redux/SignInReducer";
+import {ChangeEmail, ChangePassword, changeStatus, IUser} from "../../redux/SignInReducer";
 
 
 interface IMapStateToProps {
     data: IUser
 }
 
-const SignIn = (props: IMapStateToProps) => {
+interface IMapDispatchToProps {
+    changeStatus: (e:boolean) => void
+    ChangeEmail: (e: string) => void
+    ChangePassword: (e:string) => void
+}
+
+const SignIn = (props: IMapStateToProps & IMapDispatchToProps) => {
+
+    const onChangeStatus = (e:  ChangeEvent<HTMLInputElement>) => {
+        props.changeStatus(e.currentTarget.checked)
+    }
+
+    const onChangeEmail = (e:  ChangeEvent<HTMLInputElement>) =>{
+        props.ChangeEmail( e.currentTarget.value)
+    }
+
+    const onChangePassword = (e:  ChangeEvent<HTMLInputElement>) => {
+        props.ChangePassword(e.currentTarget.value)
+    }
+
     return (
         <div>
             <div>
-                <input type="email" placeholder={props.data.email}/>
+                <input type="email" placeholder={props.data.email} value={props.data.email} onChange={onChangeEmail}/>
             </div>
             <div>
-                <input type="password" placeholder={props.data.password}/>
+                <input type="text" placeholder={props.data.password} value={props.data.password} onChange={onChangePassword}/>
             </div>
             <div>
-                <input type="checkbox" checked={props.data.rememberMe}/> <span>Remember me</span>
+                <input type="checkbox" checked={props.data.rememberMe} onChange={onChangeStatus}/><span>Remember me</span>
+            </div>
+            <div>
+                <button>Sing In</button>
             </div>
         </div>
     );
@@ -28,4 +50,4 @@ const mapStateToProps = (state:RootState): IMapStateToProps => ({
     data: state.signIn
 })
 
-export default connect(mapStateToProps, {})(SignIn);
+export default connect(mapStateToProps, {changeStatus, ChangeEmail, ChangePassword})(SignIn);
--- src/redux/SignInReducer.ts
+++ src/redux/SignInReducer.ts
@@ -1,7 +1,24 @@
 const LOGIN_PAGE = "SingIn/LOGIN_PAGE"
+const REMEMBER_ME = "SingIn/REMEMBER_ME"
+const CHANGE_EMAIL = "SingIn/CHANGE_EMAIL"
+const CHANGE_PASSWORD = "SingIn/CHANGE_PASSWORD"
 
 interface ISignInActions {
     type: typeof LOGIN_PAGE
+}
+
+interface IRememberMe {
+    type: typeof REMEMBER_ME,
+    rememberMe: boolean
+}
+
+interface IChangeEmail {
+    type: typeof CHANGE_EMAIL,
+    email: string
+}
+interface IChangePassword {
+    type: typeof CHANGE_PASSWORD,
+    password: string
 }
 
 
@@ -12,16 +29,39 @@ export interface IUser {
 }
 
 const InitialState: IUser = {
-    email: "e",
-    password: "p",
+    email: "email",
+    password: "pfff",
     rememberMe: true
 };
 
-export const SignInReducer = (state = InitialState, action: ISignInActions) => {
+type IActions = ISignInActions
+    | IRememberMe
+    | IChangeEmail
+    | IChangePassword
+
+export const SignInReducer = (state = InitialState, action: IActions ) => {
     switch (action.type) {
         case LOGIN_PAGE: {
             return {
                 ...state
+            }
+        }
+        case REMEMBER_ME: {
+            return {
+                ...state,
+                rememberMe: action.rememberMe
+            }
+        }
+        case CHANGE_EMAIL: {
+            return {
+                ...state,
+                email: action.email
+            }
+        }
+        case CHANGE_PASSWORD: {
+            return {
+                ...state,
+                password: action.password
             }
         }
 
@@ -30,3 +70,18 @@ export const SignInReducer = (state = InitialState, action: ISignInActions) => {
         }
     }
 };
+
+export const changeStatus = (rememberMe: boolean): IRememberMe => ({
+    type: REMEMBER_ME,
+    rememberMe
+})
+
+export const ChangeEmail = (email: string): IChangeEmail => ({
+    type: CHANGE_EMAIL,
+    email
+})
+
+export const ChangePassword= (password: string): IChangePassword => ({
+    type: CHANGE_PASSWORD,
+    password
+})
